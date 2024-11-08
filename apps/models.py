99ca353f8353f90c31023678c300c7fb168dd6cb
MandePriscille <mandepriscille1@gmail.com>
2024-11-08 01:31:51 +0100

    fielt: paginations-django

--- apps/models.py
+++ apps/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=200)          
+    content = models.TextField()                      
+    author = models.CharField(max_length=150)
+    created_at = models.DateTimeField(auto_now_add=True)  
+    updated_at = models.DateTimeField(auto_now=True)
+    published = models.BooleanField(default=False)         
+
+    def __str__(self):
+        return self.title
--- apps/views.py
+++ apps/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from apps.models import Article
 
-# Create your views here.
+def get(request):
+    articles = Article.objects.all() # Récupération des articles
+    paginator = Paginator(articles, 5) # 5 articles par page
+    
+    page_number = request.GET.get('page', 1)  # Récupération de la page demandée ou de la première par défaut
+
+    try:
+        page_obj = paginator.get_page(page_number)
+
+    except PageNotAnInteger:        # Affiche la première page si le numéro de page n'est pas valide
+        page_obj = paginator.page(1)
+
+    except EmptyPage:   # Affiche la dernière page si le numéro de page est hors limite
+        page_obj = paginator.page(paginator.page_number)    
+
+    return render(request, 'paginations/page.html',{'page_obj':page_obj})
